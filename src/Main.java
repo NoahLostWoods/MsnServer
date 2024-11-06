674235b0db7573a529a03f071959fded12010d21
Valerio Carcaterra <valerio.carcaterra>
2024-11-06 10:45:38 +0100

    Pulizia progetto

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import server.MessengerServer;
-
-public class Main {
-    public static void main(String[] args) {
-        MessengerServer ms = new MessengerServer();
-        System.out.println("Il server è in ascolto sulla porta " );
-        }
-    }
